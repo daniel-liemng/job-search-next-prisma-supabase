4f04ff545b8066bab2a22d81350cead6a446b162
Daniel <daniel.liemng@gmail.com>
2023-08-10 14:57:12 -0400

    fix for deploy 2

--- app/(employer)/layout.tsx
+++ app/(employer)/layout.tsx
@@ -1,42 +1,28 @@
 'use client';
 
-import Loading from '@/components/Loading';
 import EmployerSidebar from '@/components/employer/layout/EmployerSidebar';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const EmployerLayout = ({ children }: { children: React.ReactNode }) => {
+  const { data: session } = useSession();
   const router = useRouter();
-  const { status: sessionStatus } = useSession();
-  const authorized = sessionStatus === 'authenticated';
-  const unAuthorized = sessionStatus === 'unauthenticated';
-  const loading = sessionStatus === 'loading';
 
   useEffect(() => {
-    if (loading) return;
-
-    if (unAuthorized) {
+    if (!session) {
       console.log('not authorized');
       router.push('/');
     }
-  }, [loading, unAuthorized, sessionStatus, router]);
+  }, [session, router]);
 
-  if (loading) {
-    return <Loading />;
-  }
-
-  console.log(sessionStatus);
-
-  return authorized ? (
+  return (
     <div className='flex gap-3 h-full w-full min-h-screen p-5 flex-wrap'>
       <EmployerSidebar />
       <section className='border border-gray-200 p-3 rounded-lg flex-1'>
         {children}
       </section>
     </div>
-  ) : (
-    <></>
   );
 };
 
